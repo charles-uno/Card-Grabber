cac2e87849ace56e0cb7f7b22a34f1588e0b14de
Charles <charles.a.mceachern@gmail.com>
2016-08-07 01:15:20 -0500

    added support for the 8E card face

--- builder.py
+++ builder.py
@@ -47,17 +47,18 @@ class dfc(dict):
 
     abbr, num = None, None
 
-    flip = None, None, None
+    flip = None
 
     h_ttl = 35
     h_art = 138
     h_typ = 22
-    h_txt = 80
-    h_div = 15
-    h_pow = 6
+    h_txt = 90
+    h_div = 5
+    h_lip = 5
+    h_pow = 20
 
     h_art_new = 45
-    h_txt_new = 34
+    h_txt_new = 45
 
     w_pad = 17
     w_pow = 53
@@ -67,9 +68,7 @@ class dfc(dict):
     # ==================================================================
 
     def __init__(self, abbr, num):
-
         self.abbr, self.num = abbr, str(num)
-
         # Check if we already have images for this card. If not,
         # download them. 
         for side in ('a', 'b'):
@@ -88,7 +87,7 @@ class dfc(dict):
         self.flip[:, :] = self['a'][3, 20]
         # Combine the front and back like a flip card. 
         for side in ('a', 'b'):
-            self.frame_flip(side)
+            self.draw_flip(side)
             # Flipping the canvas is easier than rearranging the pieces.
             self.flip_flip()
         return
@@ -114,49 +113,101 @@ class dfc(dict):
     # ============================================= Map Card Frames Over
     # ==================================================================
 
-    def frame_flip(self, side):
+    def draw_flip(self, side, careful=False):
+
+        art_top = self.h_ttl
+        art_bot = art_top + self.h_art
+        txt_top = art_bot + self.h_typ
+        txt_bot = txt_top + self.h_txt
+        txt_mid = txt_bot - self.h_txt_new
+        div_bot = txt_bot + self.h_div
+        pow_bot = div_bot + self.h_lip
+        pow_top = pow_bot - self.h_pow
+        art_bot_new = art_top + self.h_art_new
+        txt_top_new = art_bot_new + self.h_typ
+        txt_bot_new = txt_top_new + self.h_txt_new
+        div_bot_new = txt_bot_new + self.h_div
+        pow_bot_new = div_bot_new + self.h_lip
+        pow_top_new = pow_bot_new - self.h_pow
+
         # Title line. 
-        self.flip[:self.h_ttl, :] = self[side][:self.h_ttl, :]
-        # Left and right art framing. 
-        bot_old = self.h_ttl + self.h_art
-        top_old = bot_old - self.h_art_new
-        left = self[side][top_old:bot_old, :self.w_pad]
-        right = self[side][top_old:bot_old, -self.w_pad:]
-        bot_new = self.h_ttl + self.h_art_new
-        top_new = self.h_ttl
-        self.flip[top_new:bot_new, :self.w_pad] = left
-        self.flip[top_new:bot_new, -self.w_pad:] = right
+        self.flip[:art_top, :] = self[side][:art_top, :]
         # Type line.
-        top_old = self.h_ttl + self.h_art
-        bot_old = top_old + self.h_typ
-        top_new = self.h_ttl + self.h_art_new
-        bot_new = top_new + self.h_typ
-        self.flip[top_new:bot_new, :] = self[side][top_old:bot_old, :]
+        self.flip[art_bot_new:txt_top_new, :] = self[side][art_bot:txt_top, :]
         # Text box framing. Note that we don't use the right side. We
         # reflect the left over instead, to get rid of the divot. 
-        bot_old = self.h_ttl + self.h_art + self.h_typ + self.h_txt
-        top_old = bot_old - self.h_txt_new
-        left = self[side][top_old:bot_old, :self.w_pad]
-        top_new = self.h_ttl + self.h_art_new + self.h_typ
-        bot_new = top_new + self.h_txt_new
-        self.flip[top_new:bot_new, :self.w_pad] = left
-        self.flip[top_new:bot_new, -self.w_pad:] = left[:, ::-1]
-        # Divider at the bottom of the text box. 
-        top_old = self.h_ttl + self.h_art + self.h_typ + self.h_txt
-        bot_old = top_old + self.h_div
-        top_new = self.h_ttl + self.h_art_new + self.h_typ + self.h_txt_new
-        bot_new = top_new + self.h_div
-        self.flip[top_new:bot_new, :] = self[side][top_old:bot_old, :]
-        # Get the bottom of the power toughness box. Constrain the width
-        # so the front and back boxes can be staggered. 
-        top_old = self.h_ttl + self.h_art + self.h_typ + self.h_txt + self.h_div
-        bot_old = top_old + self.h_pow
-        temp = self[side][top_old:bot_old, -self.w_pow:]
-        top_new = self.h_ttl + self.h_art_new + self.h_typ + self.h_txt_new + self.h_div
-        bot_new = top_new + self.h_pow
-        self.flip[top_new:bot_new, -self.w_pow:] = temp
+        temp = self[side][txt_mid:pow_top, :self.w_pad]
+        self.flip[txt_top_new:pow_top_new, :self.w_pad] = temp
+        self.flip[txt_top_new:pow_top_new, -self.w_pad:] = temp[:, ::-1]
+        # Bottom of the text box, including the divider line. This risks
+        # overwriting part of the text box. 
+        if not careful:
+            self.flip[pow_top_new:div_bot_new, :] = self[side][pow_top:div_bot, :]
+        # Power toughness box. Constrain the width so the front and back
+        # boxes can be staggered. 
+        temp = self[side][pow_top:pow_bot, -self.w_pow:]
+        self.flip[pow_top_new:pow_bot_new, -self.w_pow:] = temp
+
         return
 
+    # ==================================================================
+    # ================================================== Add Art to Flip
+    # ==================================================================
+
+    def add_art(self, side, offset=None):
+        # If the position isn't given, go for the middle. 
+        if offset is None:
+            offset = (self.h_art - self.h_art_new)//2
+            print('automatic art offset:', offset)
+        # Grab the art slice. 
+        top_old = self.h_ttl + offset
+        bot_old = top_old + self.h_art_new
+        temp = self[side][top_old:bot_old, :]
+        # Flip the canvas, if necessary. 
+        if side == 'b':
+            self.flip_flip()
+        # Paste in the art slice. Note that we go all the way to the
+        # edges because Eldrazi flip cards have transparent borders. 
+        top_new = self.h_ttl
+        bot_new = top_new + self.h_art_new
+        self.flip[top_new:bot_new, :] = temp
+        # Flip the canvas back. 
+        if side == 'b':
+            self.flip_flip()
+        return
+
+    # ==================================================================
+    # ================================================= Add Text to Flip
+    # ==================================================================
+
+    def add_text(self, side, offset=None, careful=True):
+
+        # If the position isn't given, go for the middle. 
+        if offset is None:
+            offset = (self.h_txt - self.h_txt_new)//2
+            print('automatic text offset:', offset)
+        # Grab the text slice. 
+        top_old = self.h_ttl + self.h_art + self.h_typ + offset
+        bot_old = top_old + self.h_txt_new
+        temp = self[side][top_old:bot_old, self.w_pad:-self.w_pad]
+        # Flip the canvas, if necessary. 
+        if side == 'b':
+            self.flip_flip()
+        # Paste in the text slice. Note that we have already handled the
+        # borders, to preserve the curvature. 
+        top_new = self.h_ttl + self.h_art_new + self.h_typ
+        bot_new = top_new + self.h_txt_new
+        self.flip[top_new:bot_new, self.w_pad:-self.w_pad] = temp
+        # Redraw the power toughness box. Optionally, also redraw the
+        # bottom of the text box -- this can be used to wipe out
+        # truncated flavor text. 
+        self.draw_flip(side, careful=careful)
+        # Flip the canvas back. 
+        if side == 'b':
+            self.flip_flip()
+
+
+        return
 
     # ==================================================================
     # ================================================= Helper Functions
@@ -170,6 +221,57 @@ class dfc(dict):
     def shape(self, *args):
         raise TypeError('dfc.shape is read-only')
 
+    # ------------------------------------------------------------------
+
+    @property
+    def pilf(self):
+        return self.flip[::-1, ::-1]
+
+    @pilf.setter
+    def pilf(self, *args):
+        raise TypeError('dfc.pilf is read-only')
+
+    # ------------------------------------------------------------------
+
+    def debug(self):
+        shape = np.array(self.shape)*(1, 4, 1)
+        arr = np.zeros(shape)
+        width = self.shape[1]
+
+        arr[:, :width] = self['a']
+        arr[:, width:2*width] = self['b']
+        arr[:, 2*width:3*width] = self.flip
+        arr[:, 3*width:] = self.pilf
+
+        return arr        
+
+
+
+
+
+
+
+
+
+class dfc_old(dfc):
+    # In SOI, cards have the new new frame, which is black at the
+    # bottom. That's very convenient. In the original Innistrad block,
+    # cards used the 8th Edition frame. The bottom requires better
+    # matching, and the proportions are a bit different. 
+    h_ttl = 35
+    h_art = 138
+    h_typ = 21
+    h_txt = 83
+    h_div = 7
+    h_lip = 12
+    h_pow = 22
+
+    h_art_new = 43
+    h_txt_new = 43
+
+    w_pad = 17
+    w_pow = 112
+
 
 
 
@@ -180,24 +282,36 @@ class dfc(dict):
 def main():
 
     prowler = dfc('emn', 163)
+    prowler.add_art('a', 15)
+    prowler.add_art('b', 0)
+    prowler.add_text('a', 22)
+    prowler.add_text('b', 10, careful=False)
 
-    # We'll plot a triple-wide image: front, back, and flip. 
-    new_img_shape = np.array(prowler.shape)*(1, 3, 1)
-    new_img = np.zeros(new_img_shape)
-
-    # Put the front in the first slot and the back in the second slot.
-    # We'll stitch them together in the third slot. 
-    card_width = prowler.shape[1]
-    new_img[:, :card_width] = prowler['a']
-    new_img[:, card_width:2*card_width] = prowler['b']
-    new_img[:, 2*card_width:] = prowler.flip
+    waif = dfc_old('isd', 159)
+    waif.add_art('a', 10)
+    waif.add_art('b')
+    waif.add_text('a', 11)
+    waif.add_text('b', 8)
 
     # Fire up a plot figure in the proportions of three cards.
-    plt.figure( figsize=(15, 7) )
+    plt.figure( figsize=(20, 7) )
     plt.subplots_adjust(bottom=0., left=0., right=1., top=1.)
 
-    # Turn those pixels into a plot. 
-    plt.imshow(new_img)
+    # Plot the front, back, flip, and flipped flip. 
+    img = waif.debug()
+
+    blue = np.array( [0, 0, 1] )
+
+#    img[50, :] = blue
+#    img[100, :] = blue
+#    img[150, :] = blue
+#    img[200, :] = blue
+#    img[250, :] = blue
+#    img[300, :] = blue
+
+
+
+    plt.imshow(img)
 
     return plt.show()
 

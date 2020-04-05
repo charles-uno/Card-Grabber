d7cf02fecc7dc534752ad1ab9dd2e489f61cb965
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2020-04-04 22:42:23 -0500

    assemble previews end to end

--- driver.py
+++ driver.py
@@ -5,7 +5,26 @@ import flipper
 
 def main():
 
-    flipper.flip(
+    return flipper.flip(
+        "Heir of Falkenrath",
+        art_offset_0=40,
+        art_offset_1=140,
+        text_offset_0=15,
+        text_height_0=115,
+        text_offset_1=55,
+        text_height_1=50,
+    )
+
+    return flipper.flip(
+        "Lone Rider",
+        art_offset_0=0,
+        art_offset_1=0,
+        text_offset_0=0,
+        text_offset_1=50,
+        text_skew=50,
+    )
+
+    return flipper.flip(
         "Kessig Prowler",
         art_offset_0=30,
         art_offset_1=20,
@@ -15,6 +34,22 @@ def main():
         text_height_1=100,
     )
 
+    return flipper.flip(
+        "Conduit of Storms",
+        art_offset_0=140,
+        art_offset_1=100,
+        text_offset_0=60,
+        text_offset_1=55,
+    )
 
-if __name__ == '__main__':
+    return
+
+    "Grizzled Angler"
+    "Tangleclaw Werewolf"
+    "Aberrant Researcher"
+
+    return
+
+
+if __name__ == "__main__":
     main()
--- flipper/layout.py
+++ flipper/layout.py
@@ -30,9 +30,6 @@ def flip(cardname, **kwargs):
     plt.show()
 
 
-
-
-
 def combine_sides(*sides, **kwargs):
 
     assert sides[0].shape == sides[1].shape
@@ -68,9 +65,11 @@ def combine_sides(*sides, **kwargs):
         type_top_old = title_bot + art_height_old
         type_bot_old = type_top_old + type_height
 
+        text_skew = kwargs.get("text_skew", 0)*(-1)**i
+
         flank_width = 65
         flank_height_old = 310
-        flank_height_new = 130
+        flank_height_new = 135 + text_skew
 
         flank_bot_old = type_bot_old + flank_height_old
         flank_top_old = flank_bot_old - flank_height_new
@@ -78,7 +77,7 @@ def combine_sides(*sides, **kwargs):
         flank_top_new = type_bot_new
         flank_bot_new = flank_top_new + flank_height_new
 
-        foot_height = 10
+        foot_height = 15
         foot_bot_new = flank_bot_new
         foot_top_new = foot_bot_new - foot_height
 
@@ -96,8 +95,12 @@ def combine_sides(*sides, **kwargs):
         box_top_old = box_bot_old - box_height
 
         text_height_max = foot_top_new - type_bot_new
+
         text_offset_old = kwargs.get(f"text_offset_{i}", 0)
         text_height = kwargs.get(f"text_height_{i}", text_height_max)
+
+        if text_height > text_height_max:
+            print("WARNING: text height max is", text_height_max)
 
         text_offset_new = (text_height_max - text_height)//2
 
@@ -106,6 +109,9 @@ def combine_sides(*sides, **kwargs):
 
         text_top_old = type_bot_old + text_offset_old
         text_bot_old = text_top_old + text_height
+
+        print("text goes from", type_bot_old, "to", foot_top_old, ":", foot_top_old - type_bot_old)
+
 
         # Old is where it comes from on the old canvas. New is where it goes on
         # the new canvas.
@@ -130,193 +136,3 @@ def combine_sides(*sides, **kwargs):
         canvas = canvas[::-1, ::-1]
 
     return canvas
-
-
-
-
-
-class dfc_new(dict):
-
-    abbr, num = None, None
-    flip = None
-    h_ttl = 35
-    h_art = 138
-    h_typ = 22
-    h_txt = 91
-    h_div = 4
-    h_lip = 5
-    h_pow = 20
-    h_art_new = 45
-    art_offset = {"a": 0, "b": 0}
-    # Where are we trimming text from the old text box?
-    txt_top = {"a": 0, "b": 0}
-    txt_bot = {"a": 45, "b": 45}
-    # What's the size of the new text box?
-    h_txt_new = {"a": 45, "b": 45}
-    w_pad = 18
-    w_pow = 53
-
-    # ------------------------------------------------------------------
-
-    def __init__(self, abbr, num, **kwargs):
-        self.abbr, self.num = abbr, str(num)
-        for side in "ab":
-            self[side] = images.get_image(self.abbr, self.num, side)
-        assert self["a"].shape == self["b"].shape
-        # Create a third array. Fill it with the border color.
-        self.flip = np.zeros(self["a"].shape)
-        self.flip[:, :] = self["a"][3, 20]
-        # Figure out what text will go where.
-        if "txt_a" in kwargs:
-            self.txt_top["a"], self.txt_bot["a"] = kwargs.pop("txt_a")
-        if "txt_b" in kwargs:
-            self.txt_top["b"], self.txt_bot["b"] = kwargs.pop("txt_b")
-        # Optionally also say where to slice the art.
-        if "art_ab" in kwargs:
-            self.art_offset["a"], self.art_offset["b"] = kwargs.pop("art_ab")
-        # Figure out if we need to resize the text boxes.
-        h_txt = {
-            "a": self.txt_bot["a"] - self.txt_top["a"],
-            "b": self.txt_bot["b"] - self.txt_top["b"],
-        }
-        h_txt_total = self.h_txt_new["a"] + self.h_txt_new["b"]
-        # Sanity check: does all the text fit?
-        if h_txt["a"] + h_txt["b"] > h_txt_total:
-            raise ValueError("can't fit %d + %d in %d pixels" % (h_txt["a"], h_txt["b"], h_txt_total))
-        # If one side wants 60 pixels of text, shrink the other to fit.
-        for side in "ab":
-            if h_txt[side] > self.h_txt_new[side]:
-                other_side = "a" if side == "b" else "b"
-                self.h_txt_new[side] = h_txt[side]
-                self.h_txt_new[other_side] = h_txt_total - h_txt[side]
-        return self.debug() if DEBUG else self.save()
-
-    # ------------------------------------------------------------------
-
-    def __del__(self):
-        # When the object goes out of scope, clean up the plot.
-        return plt.close()
-
-    # ------------------------------------------------------------------
-
-    def draw(self):
-        for side in ('a', 'b'):
-            self.draw_art(side)
-            # Frame includes text background.
-            self.draw_frame(side)
-            self.draw_text(side)
-            self.draw_power(side)
-            self.flip_flip()
-        return
-
-    # ------------------------------------------------------------------
-
-    def save(self):
-        self.draw()
-        return images.save_image(self.flip, self.abbr, self.num, figsize=(4.46, 6.20))
-
-    # ------------------------------------------------------------------
-
-    def show(self, side):
-        self.draw()
-        return images.show_image(self[side], figsize=(4.46, 6.20))
-
-    # ------------------------------------------------------------------
-
-    def debug(self):
-        self.draw()
-        shape = np.array(self["a"].shape)*(1, 4, 1)
-        arr = np.zeros(shape)
-        width = self["a"].shape[1]
-        arr[:, :width] = self['a']
-        arr[:, width:2*width] = self['b']
-        arr[:, 2*width:3*width] = self.flip
-        arr[:, 3*width:] = self.flip[::-1, ::-1]
-        images.show_image(arr, figsize=(19.84, 6.20))
-        # Debug one thing at a time.
-        return sys.exit()
-
-    # ------------------------------------------------------------------
-
-    def flip_flip(self):
-        self.flip = self.flip[::-1, ::-1]
-
-    # ------------------------------------------------------------------
-
-    def draw_power(self, side):
-        art_top = self.h_ttl
-        art_bot = art_top + self.h_art
-        txt_top = art_bot + self.h_typ
-        txt_bot = txt_top + self.h_txt
-        div_bot = txt_bot + self.h_div
-        pow_bot = div_bot + self.h_lip
-        pow_top = pow_bot - self.h_pow
-        art_bot_new = art_top + self.h_art_new
-        txt_top_new = art_bot_new + self.h_typ
-        txt_bot_new = txt_top_new + self.h_txt_new[side]
-        div_bot_new = txt_bot_new + self.h_div
-        pow_bot_new = div_bot_new + self.h_lip
-        pow_top_new = pow_bot_new - self.h_pow
-        self.flip[pow_top_new:pow_bot_new, -self.w_pow:] = self[side][pow_top:pow_bot, -self.w_pow:]
-        return
-
-    # ------------------------------------------------------------------
-
-    def draw_frame(self, side):
-        art_top = self.h_ttl
-        art_bot = art_top + self.h_art
-        txt_top = art_bot + self.h_typ
-        txt_bot = txt_top + self.h_txt
-        txt_mid = txt_bot - self.h_txt_new[side]
-        div_bot = txt_bot + self.h_div
-        pow_bot = div_bot + self.h_lip
-        pow_top = pow_bot - self.h_pow
-        art_bot_new = art_top + self.h_art_new
-        txt_top_new = art_bot_new + self.h_typ
-        txt_bot_new = txt_top_new + self.h_txt_new[side]
-        div_bot_new = txt_bot_new + self.h_div
-        pow_bot_new = div_bot_new + self.h_lip
-        pow_top_new = pow_bot_new - self.h_pow
-        # Title line.
-        self.flip[:art_top, :] = self[side][:art_top, :]
-        # Type line.
-        self.flip[art_bot_new:txt_top_new, :] = self[side][art_bot:txt_top, :]
-        # Text box framing. Reflect the left over to lose the divot.
-        temp = self[side][txt_mid:pow_top, :self.w_pad]
-        self.flip[txt_top_new:pow_top_new, :self.w_pad] = temp
-        self.flip[txt_top_new:pow_top_new, -self.w_pad:] = temp[:, ::-1]
-        # Right and bottom borders of the text box.
-        self.flip[pow_top_new:div_bot_new, :self.w_pad] = self[side][pow_top:div_bot, :self.w_pad]
-        self.flip[txt_bot_new:div_bot_new, self.w_pad:] = self[side][txt_bot:div_bot, self.w_pad + 1:self.w_pad + 2, :]
-        # Empty background for the text box.
-        text_box_bg = [1, 0, 0] if DEBUG else self[side][txt_bot - 2, self.w_pad + 1]
-        self.flip[txt_top_new:txt_bot_new, self.w_pad:-self.w_pad] = text_box_bg
-        return
-
-    # ------------------------------------------------------------------
-
-    def draw_art(self, side):
-        offset = self.art_offset[side]
-        top_old = self.h_ttl + offset
-        bot_old = top_old + self.h_art_new
-        temp = self[side][top_old:bot_old, :]
-        # Handle full-width art so transparent borders look good.
-        top_new = self.h_ttl
-        bot_new = top_new + self.h_art_new
-        self.flip[top_new:bot_new, :] = temp
-        return
-
-    # ------------------------------------------------------------------
-
-    def draw_text(self, side):
-        # Grab the chunk of text. Top and bottom are configurable.
-        top_old = self.h_ttl + self.h_art + self.h_typ + self.txt_top[side]
-        bot_old = top_old - self.txt_top[side] + self.txt_bot[side]
-        # Figure out where it's going to go.
-        pad = (self.h_txt_new[side] - bot_old + top_old)//2
-        if pad < 0:
-            raise ValueError("calculated negative padding for the textbox!")
-        top_new = self.h_ttl + self.h_art_new + self.h_typ + pad
-        bot_new = top_new + (bot_old - top_old)
-        self.flip[top_new:bot_new, self.w_pad:-self.w_pad] = self[side][top_old:bot_old, self.w_pad:-self.w_pad]
-        return
